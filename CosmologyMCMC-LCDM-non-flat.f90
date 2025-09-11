8bbe7c2590f618514b1a8a0dd01bee20e783fe28
krezazadeh <65955905+krezazadeh@users.noreply.github.com>
2025-09-11 10:59:26 +0330

    Add files via upload

--- CosmologyMCMC-LCDM-non-flat.f90
+++ CosmologyMCMC-LCDM-non-flat.f90
@@ -81,7 +81,7 @@ program CosmologyMCMC
     ! real(8), parameter :: ok = 0.0d0
     
     ! Number of varying parameters of the model
-    integer, parameter :: nparams = 3
+    integer, parameter :: nparams = 4
     
     ! Varying parameters of the model
     real(8) :: h, ob, oc
@@ -190,10 +190,10 @@ program CosmologyMCMC
     
     call random_seed()
 
-    initpoint = (/ 0.688993d0, 0.2511d0, -0.003d0 /)
-    jumpsize = (/ 0.001d0, 0.001d0, 0.001d0 /)
-    priormin = (/ 0.6d0, 0.1d0, -0.1d0 /)
-    priormax = (/ 0.8d0, 0.5d0, 0.1d0 /)
+    initpoint = (/ 0.688993d0, 0.047d0, 0.2511d0, 0.003d0 /)
+    jumpsize = (/ 0.001d0, 0.001d0, 0.001d0, 0.001d0 /)
+    priormin = (/ 0.6d0, 0.01d0, 0.1d0, -0.1d0 /)
+    priormax = (/ 0.8d0, 0.1d0, 0.5d0, 0.1d0 /)
 
     ! start from the given initial point.
     do i = 1, nparams
@@ -207,9 +207,9 @@ program CosmologyMCMC
     ! end do
 
     h = params(1)
-    ob = 0.02218d0/h**2
-    oc = params(2)
-    ok = params(3)
+    ob = params(2)
+    oc = params(3)
+    ok = params(4)
     call DLsol()
     chi2params = chi2total()
 
@@ -241,16 +241,16 @@ program CosmologyMCMC
         end do
 
         h = params_new(1)
-        ob = 0.02218d0/h**2
-        oc = params(2)
-        ok = params_new(3)
+        ob = params(2)
+        oc = params(3)
+        ok = params_new(4)
         call DLsol()
         chi2params_new = chi2total()
 
         points_local(rank + 1, i, :) = params_new
 
         write(10 + rank, "(10e25.16)") 1.0d0, chi2params_new/2.0d0, params_new, &
-        ob, H0(), om(), ol(), age()
+        H0(), om(), ol(), age()
 
         alpha = min(1.0d0, exp(-0.5d0 * (chi2params_new - chi2params)))
         call random_number(rand)
